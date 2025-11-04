65361c1bc7d991177c307d41ec4ca1c7591675ff
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-11-04 11:22:39 +0700

    nambah config.js, film.js, index.js dan ss db

---
+++ models/film.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+    const film = sequelize.define('film', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        nama_film: {
+            type: DataTypes.STRING,
+        },
+        deskripsi: {
+            type: DataTypes.TEXT,
+        },
+        sutradara: {
+            type: DataTypes.STRING,
+        },
+        tahun_terbit: {
+            type: DataTypes.INTEGER,
+        },
+        genre: {
+            type: DataTypes.STRING,
+        },
+    });
+
+    return film;
+};
